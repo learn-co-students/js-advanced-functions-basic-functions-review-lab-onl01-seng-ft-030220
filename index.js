8b7fc22a1e1cd5470c10800ede8dca6b1d7df535
Craig Christopher Messi <craig.messi@gmail.com>
2020-06-23 18:19:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const saturdayFun = function(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!` 
+}
+
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.` 
+}
+
+const wrapAdjective = function(highlight = '*') {
+  return function(adj = 'special') {
+    return `You are ${highlight}${adj}${highlight}!`
+  }
+}
+
+const Calculator = {
+  add: function(num1, num2) {
+    return num1 + num2
+  },
+  
+  subtract: function(num1, num2) {
+    return num1 - num2
+  },
+  
+  multiply: function(num1, num2) {
+    return num1 * num2
+  },
+  
+  divide: function(num1, num2) {
+    return num1 / num2
+  }
+}
+
+const actionApplyer = function(integer, actions) {
+  return actions.reduce((total, currentAction) => {
+    return currentAction(total)
+  }, integer)
+}
